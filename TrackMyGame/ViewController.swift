481aff899c3416a9a44dca48c24948ddf39022fd
ricardolopes86 <ricardolopes18@yahoo.com.br>
2016-02-17 10:35:09 -0200

    Initial Commit

---
+++ TrackMyGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrackMyGame
+//
+//  Created by Ricardo Lopes on 2/17/16.
+//  Copyright © 2016 Ricardo Lopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
